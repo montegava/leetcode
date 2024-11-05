22d55c236aeb94e51f03928df8d30434ed8f1bc3
vyachesla.demydenko <vyachesla.demydenko>
2024-11-05 21:22:27 +0100

    fix

--- ML.Week3/Program.cs
+++ ML.Week3/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+
+using ML.Week3.maximum_width_of_binary_tree;
+using Week1.common;
+
+
+//new Solution().WidthOfBinaryTree(BinaryTree.BuildTree(new int?[]{ 1, 3, 2, 5, null, null, 9, 6, null, 7 }));
+
+//new ML.Week3.shortest_unsorted_continuous_subarray.Solution().FindUnsortedSubarray(new []{ 2, 6, 4, 8, 10, 9, 15 });
+new ML.Week3.shortest_unsorted_continuous_subarray.Solution().FindUnsortedSubarray(new []{ 1, 3, 2, 4, 5 });
+
 Console.WriteLine("Hello, World!");
---
+++ ML.Week3/shortest-unsorted-continuous-subarray/Solution.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML.Week3.shortest_unsorted_continuous_subarray
+{
+    public class Solution
+    {
+        public int FindUnsortedSubarray(int[] nums)
+        {
+            // 3,1,2
+            // 2,6,4,8,1,9,15
+            // s
+            //         e
+            // sort
+            // 
+
+            var desc = true;
+            var ask = true;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                desc = desc && nums[i] < nums[i - 1];
+                ask = ask && nums[i] > nums[i - 1];
+            }
+
+            if (ask)
+                return 0;
+
+            if (desc)
+                return nums.Length;
+
+            // 1,3,2,4,5
+
+            var s = 0;
+            var e = 1;
+            var max = nums.Length;
+            var sorted = true;
+            while (s <= e && s < nums.Length && e < nums.Length)
+            {
+                if (nums[s] > nums[e])
+                {
+                    max = Math.Min(max, e - s - 1);
+                    s++;
+                    e = s + 1;
+                }
+                else
+                {
+                    sorted = sorted &&  nums[e] > nums[e - 1];
+                    e++;
+
+                    if (e == nums.Length && !sorted) 
+                      max = Math.Min(max, e - s - 2);
+
+                }
+            }
+
+            
+
+            return max;
+        }
+    }
+}
