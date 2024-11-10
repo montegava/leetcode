a6db51388add03a60e2f159c7d56e33ec81a021f
vyachesla.demydenko <vyachesla.demydenko>
2024-11-10 17:43:28 +0100

    #3 solved

--- ML.Week4/Program.cs
+++ ML.Week4/Program.cs
@@ -1,7 +1,10 @@
 ﻿
-using ML.Week4.minimum_array_end;
 
-new Solution().MinEnd(2, 7);
+//new Solution().MinEnd(2, 7);
+
+using Week1.common;
+
+new ML.Week4.delete_node_in_a_bst.Solution().DeleteNode(BinaryTree.BuildTree(new int?[] { 3, 1, 4, null, 2 }), 1);
 
 
 Console.WriteLine("Hello, World!");
---
+++ ML.Week4/delete-node-in-a-bst/Solution.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ML.Common.common;
+using Week1.common;
+
+namespace ML.Week4.delete_node_in_a_bst
+{
+    public class Solution
+    {
+        public TreeNode DeleteNode(TreeNode root, int key)
+        {
+
+            (TreeNode d, TreeNode p) dfs(TreeNode n, TreeNode p)
+            {
+                if (n == null)
+                    return (null, null);
+
+                if (n.val == key)
+                    return (n, p);
+
+                if (n.val > key)
+                    return dfs(n.left, n);
+
+                return dfs(n.right, n);
+            }
+
+            void dfs2(TreeNode parent, TreeNode attach)
+            {
+                if (parent == null || attach == null)
+                    return;
+
+                if (parent.val > attach.val)
+                {
+                    if (parent.left == null)
+                    {
+                        parent.left = attach;
+                        return;
+                    }
+                    else
+                    {
+                        dfs2(parent.left, attach);
+                    }
+                }
+                if (parent.val < attach.val)
+                {
+                    if (parent.right == null)
+                    {
+                        parent.right = attach;
+                        return;
+                    }
+                    else
+                    {
+                        dfs2(parent.right, attach);
+                    }
+                }
+
+            }
+
+
+            var res = dfs(root, null);
+
+            if (res.d == null)
+                return root;
+
+            if (res.p == null)
+            {
+                if (res.d.right != null)
+                {
+                    dfs2(res.d.right, res.d.left);
+                    return res.d.right;
+                }
+                else
+                {
+                    dfs2(res.d.left, res.d.right);
+                    return res.d.left;
+                }
+            }
+            else
+            {
+                if (res.p.right == res.d)
+                {
+                    if (res.d.right != null)
+                    {
+                        res.p.right = res.d.right;
+                        dfs2(res.d.right, res.d.left);
+                    }
+                    else
+                    {
+                        res.p.right = res.d.left;
+                        dfs2(res.d.right, res.d.right);
+                    }
+                }
+                else
+                {
+                    if (res.d.left != null)
+                    {
+                        res.p.left = res.d.left;
+                        dfs2(res.d.left, res.d.right);
+                    }
+                    else
+                    {
+                        res.p.left = res.d.right;
+                        dfs2(res.d.left, res.d.left);
+                    }
+                }
+
+            }
+
+            return root;
+        }
+    }
+}
