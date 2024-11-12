2e9cebe12a910172ce1e538b9dd7713c3e8c0313
vyachesla.demydenko <vyachesla.demydenko>
2024-11-12 15:46:24 +0100

    re

--- ML.Week4/Program.cs
+++ ML.Week4/Program.cs
@@ -9,7 +9,9 @@ using ML.Week4.minimum_height_trees;using Week1.common;using Solution = ML.Week4
 //new Solution().FindMinHeightTrees(6, new int[][] { [0, 1], [0, 2], [0, 3], [3, 4], [4, 5] });
 
 
-new Solution().Connect(BinaryTree.BuildTree(new int?[] { 1, 2, 3, 4, 5, 6, 7 }));
+//new Solution().Connect(BinaryTree.BuildTree(new int?[] { 1, 2, 3, 4, 5, 6, 7 }));
+new ML.Week4.number_of_provinces.Solution().FindCircleNum(new int[][]
+    { [1, 0, 0, 1], [0, 1, 1, 0], [0, 1, 1, 1], [1, 0, 1, 1] });
 
 
 
---
+++ ML.Week4/max-chunks-to-make-sorted/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML.Week4.max_chunks_to_make_sorted
+{
+    public class Solution
+    {
+        public int MaxChunksToSorted(int[] arr)
+        {
+            var result = 0;
+            var max = int.MinValue;
+            //1,0,2,3,4
+            //1,1,2,3,4
+            for (int i = 0; i < arr.Length; i++)
+            {
+                
+                max = Math.Max(max, arr[i]);
+
+                if (max == i)
+                    result++;
+            }
+
+            return result;
+
+
+
+        }
+    }
+}
